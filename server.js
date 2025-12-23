50a96f454b3c4ee3d9157eb3370a458dc9b2a3d2
Pritesh31Gupta <pritesh31kumar@gmail.com>
2025-12-23 19:50:54 +0530

    ashish

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 
  app.use(cors({
-  origin: 'https://enchanting-sopapillas-73dedd.netlify.app/',
+  origin: 'https://enchanting-sopapillas-73dedd.netlify.app',
   credentials: true
 }));
 app.use(express.json());
